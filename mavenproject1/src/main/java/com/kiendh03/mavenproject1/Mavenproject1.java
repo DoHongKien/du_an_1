0c0a421dd460c09f062eff22c76932c85f7aa004
kien <kiendhph21883@fpt.edu.vn>
2022-11-17 14:37:02 +0700

    update

--- mavenproject1/src/main/java/com/kiendh03/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/kiendh03/mavenproject1/Mavenproject1.java
@@ -14,5 +14,7 @@ public class Mavenproject1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World2!");
+        
+        //Test 1
     }
 }
